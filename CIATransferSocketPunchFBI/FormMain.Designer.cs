0150c3c7fcc99b97525b2c0bb0050b978a82285a
qinyi <120742915@qq.com>
2018-06-08 15:28:18 +0800

    ui are the same with java version. can transfer 1 cia file success transfer fifo was implemented,but didn't test

---
+++ CIATransferSocketPunchFBI/FormMain.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CIATransferSocketPunchFBI
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnStartTransfer = new System.Windows.Forms.Button();
+			this.listAllCIAs = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.listCIAsToTransfer = new System.Windows.Forms.ListBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtIpAddress = new System.Windows.Forms.TextBox();
+			this.progress = new System.Windows.Forms.ProgressBar();
+			this.lbState = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnStartTransfer
+			// 
+			this.btnStartTransfer.Location = new System.Drawing.Point(150, 328);
+			this.btnStartTransfer.Name = "btnStartTransfer";
+			this.btnStartTransfer.Size = new System.Drawing.Size(75, 23);
+			this.btnStartTransfer.TabIndex = 0;
+			this.btnStartTransfer.Text = "开始传输";
+			this.btnStartTransfer.UseVisualStyleBackColor = true;
+			this.btnStartTransfer.Click += new System.EventHandler(this.btnStartTransfer_Click);
+			// 
+			// listAllCIAs
+			// 
+			this.listAllCIAs.FormattingEnabled = true;
+			this.listAllCIAs.ItemHeight = 12;
+			this.listAllCIAs.Location = new System.Drawing.Point(14, 24);
+			this.listAllCIAs.Name = "listAllCIAs";
+			this.listAllCIAs.Size = new System.Drawing.Size(519, 112);
+			this.listAllCIAs.TabIndex = 1;
+			this.listAllCIAs.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listAllCIAs_MouseDoubleClick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(167, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "CIA文件（双击加入传输队列）";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 161);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(191, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "CIA传输队列（双击移除传输队列）";
+			// 
+			// listCIAsToTransfer
+			// 
+			this.listCIAsToTransfer.FormattingEnabled = true;
+			this.listCIAsToTransfer.ItemHeight = 12;
+			this.listCIAsToTransfer.Location = new System.Drawing.Point(14, 176);
+			this.listCIAsToTransfer.Name = "listCIAsToTransfer";
+			this.listCIAsToTransfer.Size = new System.Drawing.Size(519, 112);
+			this.listCIAsToTransfer.TabIndex = 4;
+			this.listCIAsToTransfer.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listCIAsToTransfer_MouseDoubleClick);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 306);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(125, 12);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Ip地址（设备上显示）";
+			// 
+			// txtIpAddress
+			// 
+			this.txtIpAddress.Location = new System.Drawing.Point(14, 330);
+			this.txtIpAddress.Name = "txtIpAddress";
+			this.txtIpAddress.Size = new System.Drawing.Size(130, 21);
+			this.txtIpAddress.TabIndex = 6;
+			this.txtIpAddress.Text = "192.168.55.35";
+			// 
+			// progress
+			// 
+			this.progress.Location = new System.Drawing.Point(14, 357);
+			this.progress.Name = "progress";
+			this.progress.Size = new System.Drawing.Size(521, 23);
+			this.progress.TabIndex = 7;
+			// 
+			// lbState
+			// 
+			this.lbState.Location = new System.Drawing.Point(289, 333);
+			this.lbState.Name = "lbState";
+			this.lbState.Size = new System.Drawing.Size(244, 12);
+			this.lbState.TabIndex = 8;
+			this.lbState.Text = "0.0Mb of 0.0Mb @ 0.0%";
+			this.lbState.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(545, 523);
+			this.Controls.Add(this.lbState);
+			this.Controls.Add(this.progress);
+			this.Controls.Add(this.txtIpAddress);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.listCIAsToTransfer);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.listAllCIAs);
+			this.Controls.Add(this.btnStartTransfer);
+			this.Name = "FormMain";
+			this.Text = "SocketPunch-Just.Qin";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStartTransfer;
+		private System.Windows.Forms.ListBox listAllCIAs;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ListBox listCIAsToTransfer;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtIpAddress;
+		private System.Windows.Forms.ProgressBar progress;
+		private System.Windows.Forms.Label lbState;
+	}
+}
+
---
+++ CIATransferSocketPunchFBI/FormMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CIATransferSocketPunchFBI
+{
+	public partial class FormMain : Form
+	{
+		private Socket socket;
+		List<string> ciaFiles;
+		private List<string> ciaToTransfers = new List<string>();
+		private void ListAllCIAs()
+		{
+			ciaFiles = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.cia", SearchOption.AllDirectories).ToList();
+			if (ciaFiles == null || ciaFiles.Count == 0)
+			{
+				listAllCIAs.Items.Add("没有找到CIA文件");
+			}
+			for (int i = 0; i < ciaFiles.Count; i++)
+			{
+				listAllCIAs.Items.Add(Path.GetFileName(ciaFiles[i]));
+			}
+		}
+
+		public FormMain()
+		{
+			InitializeComponent();
+			ListAllCIAs();
+		}
+
+		void ThreadTransfer(object ip)
+		{
+			try
+			{
+				byte[] buffer = new byte[256 * 1024];
+				for (int i = 0; i < ciaToTransfers.Count; i++)
+				{
+					if (!socket.Connected)
+					{
+						socket.Connect(ip.ToString(), 5000);
+					}
+					FileStream stream = new FileStream(ciaToTransfers[i], FileMode.Open);
+
+					BinaryReader fileReader = new BinaryReader(stream);
+					long fileTotalLength = stream.Length;
+					buffer[0] = (byte) (fileTotalLength >> 56);
+					buffer[1] = (byte) (fileTotalLength >> 48);
+					buffer[2] = (byte) (fileTotalLength >> 40);
+					buffer[3] = (byte) (fileTotalLength >> 32);
+					buffer[4] = (byte) (fileTotalLength >> 24);
+					buffer[5] = (byte) (fileTotalLength >> 16);
+					buffer[6] = (byte) (fileTotalLength >> 8);
+					buffer[7] = (byte) (fileTotalLength >> 0);
+					socket.Send(buffer, 0, 8, SocketFlags.DontRoute);
+					this.Invoke(new MethodInvoker(delegate
+					{
+						listCIAsToTransfer.SelectedIndex = i;
+						progress.Value = 0;
+						progress.Maximum = (int) fileTotalLength;
+						lbState.Text = string.Format("{0:F2}Mb of {1:F2}Mb @ {2:F2}%", 0, 0, 0);
+					}));
+					//Thread.Sleep(5000);
+					int readLength;
+					long sendLength = 0;
+					double fileMb = fileTotalLength / (1024.0 * 1024.0);
+					while ((readLength = fileReader.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						sendLength += readLength;
+						//Console.WriteLine("{0}/{1},{2}", sendLength, fileTotalLength, sendLength * 100.0 / fileTotalLength);
+						if (readLength != buffer.Length)
+						{
+							socket.Send(buffer, 0, readLength, SocketFlags.DontRoute);
+						}
+						else
+							socket.Send(buffer, SocketFlags.DontRoute);
+						this.Invoke(new MethodInvoker(delegate
+						{
+							lbState.Text = string.Format("{0:F2}Mb of {1:F2}Mb @ {2:F2}%", sendLength / (1024.0 * 1024.0), fileMb,
+								sendLength * 100.0 / fileTotalLength);
+							progress.Value = (int) sendLength;
+						}));
+					}
+					fileReader.Close();
+					stream.Close();
+					Console.WriteLine("tans complete");
+					Thread.Sleep(2000);
+				}
+
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+			finally
+			{
+				this.Invoke(new MethodInvoker(delegate
+				{
+					btnStartTransfer.Enabled = true;
+				}));
+			}
+		}
+
+		private void btnStartTransfer_Click(object sender, EventArgs e)
+		{
+			socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			new Thread(ThreadTransfer, 0).Start(txtIpAddress.Text);
+			btnStartTransfer.Enabled = false;
+		}
+
+		private void listAllCIAs_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			int index = listAllCIAs.IndexFromPoint(e.Location);
+			if (index != ListBox.NoMatches)
+			{
+				if (ciaToTransfers.Contains(ciaFiles[index]))
+				{
+					return;
+				}
+				ciaToTransfers.Add(ciaFiles[index]);
+				listCIAsToTransfer.Items.Add(Path.GetFileName(ciaFiles[index]));
+			}
+		}
+
+		private void listCIAsToTransfer_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			int index = listCIAsToTransfer.IndexFromPoint(e.Location);
+			if (index != ListBox.NoMatches)
+			{
+				ciaToTransfers.RemoveAt(index);
+				listCIAsToTransfer.Items.RemoveAt(index);
+			}
+		}
+	}
+}
